e038438f3b5ffe026af15ff114863603e9d1e01c
Kamil Chlebuś <kamil.chlebus@etteplan.com>
2023-09-01 12:09:17 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Kamil Chlebuś on 01/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
